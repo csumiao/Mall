2773c1be14b7b386886341532534af47a1b56695
csumiao <csumiao@163.com>
2018-07-03 18:37:53 +0800

    广告内容后台的发布

--- mall-manager-web/src/main/webapp/js/controller/goodsController.js
+++ mall-manager-web/src/main/webapp/js/controller/goodsController.js
@@ -104,5 +104,20 @@ app.controller('goodsController' ,function($scope, $controller, goodsService, it
 		);
 	}
 	
+	/**
+	 * 上传图片
+	 */
+	$scope.uploadFile=function(){	  
+		uploadService.uploadFile().success(function(response) {        	
+        	if(response.success){//如果上传成功，取出url
+        		$scope.image_entity.url=response.message;//设置文件地址
+        	}else{
+        		alert(response.message);
+        	}
+        }).error(function() {           
+        	     alert("上传发生错误");
+        });        
+    };
+	
     
 });	
